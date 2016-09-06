bc60ba40809163670885a5f8981d040eba7b9b3b
Robert Sköld <robert@publicclass.se>
2016-09-06 10:49:50 +0200

    Using a simple tree instead of relying on vdom for elements test

--- test/elements.js
+++ test/elements.js
@@ -1,98 +1,114 @@
 var test = require('tape')
 var vdom = require('virtual-dom')
 var hyperx = require('../')
-var hx = hyperx(vdom.h)
+var hx = hyperx(tree)
+
+function tree(tagName, attr, children) {
+  return [tagName, attr, children || []]
+}
 
 test('div', function (t) {
-  var expected = `<div></div>`
-  var tree = hx`<div></div>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['div', {}, []]
+  var actual = hx`<div></div>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('div selfclosing', function (t) {
-  var expected = `<div></div>`
-  var tree = hx`<div />`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['div', {}, []]
+  var actual = hx`<div />`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('div > span selfclosing', function (t) {
-  var expected = `<div><span></span></div>`
-  var tree = hx`<div><span /></div>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['div', {}, [['span', {}, []]]]
+  var actual = hx`<div><span /></div>`
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('div > multi span', function (t) {
+  var expected = ['div', {}, [['span', {}, []], ['span', {}, []]]]
+  var actual = hx`<div><span></span><span></span></div>`
+  t.deepEqual(actual, expected)
+  t.end()
+})
+
+test('div > multi div', function (t) {
+  var expected = ['div', {}, [['div', {}, []], ['div', {}, []]]]
+  var actual = hx`<div><div></div><div></div></div>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('div > multi span selfclosing', function (t) {
-  var expected = `<div><span></span><span></span></div>`
-  var tree = hx`<div><span /><span /></div>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['div', {}, [['span', {}, []], ['span', {}, []]]]
+  var actual = hx`<div><span /><span /></div>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('div > multi div selfclosing', function (t) {
-  var expected = `<div><div></div><div></div></div>`
-  var tree = hx`<div><div /><div /></div>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['div', {}, [['div', {}, []], ['div', {}, []]]]
+  var actual = hx`<div><div /><div /></div>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('tag-list', function (t) {
-  var expected = `<tag-list></tag-list>`
-  var tree = hx`<tag-list></tag-list>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['tag-list', {}, []]
+  var actual = hx`<tag-list></tag-list>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('tag-list selfclosing', function (t) {
-  var expected = `<tag-list></tag-list>`
-  var tree = hx`<tag-list />`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['tag-list', {}, []]
+  var actual = hx`<tag-list />`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('tag-list > multi div selfclosing', function (t) {
-  var expected = `<tag-list><div></div><div></div><div></div><div></div></tag-list>`
-  var tree = hx`<tag-list><div /><div /><div /><div /></tag-list>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['tag-list', {}, [['div', {}, []], ['div', {}, []]]]
+  var actual = hx`<tag-list />`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('tag-list > multi tag selfclosing', function (t) {
-  var expected = `<tag-list><tag></tag><tag></tag><tag></tag><tag></tag></tag-list>`
-  var tree = hx`<tag-list><tag /><tag /><tag /><tag /></tag-list>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['tag-list', {}, [['tag', {}, []], ['tag', {}, []]]]
+  var actual = hx`<tag-list><tag /><tag /></tag-list>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
-
 test('TagList', function (t) {
-  var expected = `<TagList></TagList>`
-  var tree = hx`<TagList></TagList>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['TagList', {}, []]
+  var actual = hx`<TagList></TagList>`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('TagList selfclosing', function (t) {
-  var expected = `<TagList></TagList>`
-  var tree = hx`<TagList />`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['TagList', {}, []]
+  var actual = hx`<TagList />`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('TagList > multi div selfclosing', function (t) {
-  var expected = `<TagList><div></div><div></div><div></div><div></div></TagList>`
-  var tree = hx`<TagList><div /><div /><div /><div /></TagList>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['TagList', {}, [['div', {}, []], ['div', {}, []]]]
+  var actual = hx`<TagList />`
+  t.deepEqual(actual, expected)
   t.end()
 })
 
 test('TagList > multi Tag selfclosing', function (t) {
-  var expected = `<TagList><Tag></Tag><Tag></Tag><Tag></Tag><Tag></Tag></TagList>`
-  var tree = hx`<TagList><Tag /><Tag /><Tag /><Tag /></TagList>`
-  t.equal(vdom.create(tree).toString(), expected)
+  var expected = ['TagList', {}, [['Tag', {}, []], ['Tag', {}, []]]]
+  var actual = hx`<TagList><Tag /><Tag /></TagList>`
+  t.deepEqual(actual, expected)
   t.end()
 })
-
 
